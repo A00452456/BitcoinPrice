2358c91402342f594cc307d62c6393ce6021610c
Aamir Faruk Mansuri <90584122+A00452456@users.noreply.github.com>
2022-04-12 16:40:51 -0300

    Formatting and removing comments

--- bitcoin.py
+++ bitcoin.py
@@ -2,18 +2,17 @@ import streamlit as st
 import pandas as pd
 import requests
 import plotly.express as px
-#import plotly.graph_objects as go
 
 days = st.slider('No of days', 1, 365, 90)
 currency = st.radio(
-     "Currency",
-     ('cad', 'usd', 'inr'))
-API_URL = 'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency={}&days={}&interval=daily'.format( currency,days )
+    "Currency",
+    ('cad', 'usd', 'inr'))
+API_URL = 'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency={}&days={}&interval=daily'.format(
+    currency, days)
 req = requests.get(API_URL)
 if req.status_code == 200:
     data = req.json()
-    #print(data)
-    
+
 raw_data = data['prices']
 date_list = []
 price_list = []
@@ -22,17 +21,18 @@ for rows in raw_data:
     price_list.append(rows[1])
 
 df = pd.DataFrame(dict(
-    Date = date_list,
-    Price = price_list
+    Date=date_list,
+    Price=price_list
 ))
 
-graph = px.line(        
-        df, 
-        x = "Date",
-        y = "Price",
-        title = "Line frame"
-    )
-graph.update_traces(line_color = "white")
+graph = px.line(
+    df,
+    x="Date",
+    y="Price",
+    title="Line frame"
+)
+graph.update_traces(line_color="white")
 st.plotly_chart(graph)
 
-st.write("Average price during this time was {} cad".format(sum(price_list)/len(price_list)))
+st.write("Average price during this time was {} {}".format(
+    sum(price_list)/len(price_list), currency))
